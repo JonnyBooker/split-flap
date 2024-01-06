dbd177b5b3a2eb054b37fcf3fc6560e2fa630f68
Jonny Booker <1131478+JonnyBooker@users.noreply.github.com>
2024-01-06 18:02:14 +0000

    Initial work(around?) to make wifi manager play nice with web server

--- ESPMaster/ServiceWifiFunctions.ino
+++ ESPMaster/ServiceWifiFunctions.ino
@@ -13,7 +13,21 @@ void initWiFi() {
   wifiManager.setShowInfoUpdate(false);
   wifiManager.setConfigPortalBlocking(true);
   wifiManager.setConfigPortalTimeout(wifiConnectTimeoutSeconds);
-  wifiManager.setConnectTimeout(60);
+  wifiManager.setConnectTimeout(60);    
+  wifiManager.setWiFiAutoReconnect(true);
+  wifiManager.setSaveConfigCallback([]() {
+    //Sadly, if we've had to open up the WiFi manager portal to set the WiFi configuration up
+    //then a soft reset is necessary. There looks to be issues with the ESP and running
+    //a webserver alongside the WiFiManager. Some reading around this issue here suggests it is
+    //not possible to fix:
+    //https://github.com/tzapu/WiFiManager/issues/1579
+    
+    //Suggestion to fix is reset the device:
+    //https://github.com/rancilio-pid/clevercoffee/issues/323#issuecomment-1587344185
+
+    SerialPrintln("New WiFi configuration saved. Will need to reboot device to let webserver work...");
+    isPendingReboot = true;
+  });
   
   //Set the menu options
   std::vector<const char *> menu = { "wifi", "info" ,"param", "sep", "restart", "exit" };
